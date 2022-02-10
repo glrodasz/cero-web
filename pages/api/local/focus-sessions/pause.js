7e84112d5052984186307cf37079a19f1f4d517c
Guillermo Rodas <me@guillermorodas.com>
2022-02-11 00:00:43 +0100

    feat(focus-session): support timed pauses

--- pages/api/local/focus-sessions/pause.js
+++ pages/api/local/focus-sessions/pause.js
@@ -2,6 +2,7 @@ import crypto from 'crypto'
 
 import buildLocalApiUrl from '../../../../utils/buildLocalApiUrl'
 import fetchJsonServer from '../../../../utils/fetchJsonServer'
+import { resumeActiveFocusSession } from './resume'
 
 async function getActiveFocusSession({ options }) {
   const fetchOptions = {
@@ -18,11 +19,18 @@ async function getActiveFocusSession({ options }) {
   })
 }
 
-async function updateActiveFocusSession({ activeFocusSession, options, res }) {
-  const currentPauses = activeFocusSession.pauses ?? []
+async function pauseActiveFocusSession({ activeFocusSession, options, res }) {
+  const { time } = options.body
+  let currentPauses = activeFocusSession.pauses ?? []
   const activePause = currentPauses.find((pause) => pause.endTime === null)
 
-  if (activePause) {
+  if (activePause && time) {
+    const { pauses } = await resumeActiveFocusSession({
+      activeFocusSession,
+      options,
+    })
+    currentPauses = pauses
+  } else if (activePause) {
     return res.status(200).json(activeFocusSession)
   }
 
@@ -30,6 +38,7 @@ async function updateActiveFocusSession({ activeFocusSession, options, res }) {
     id: crypto.randomUUID(),
     startTime: Date.now(),
     endTime: null,
+    time: time ? Number(time) : undefined,
   }
 
   const fetchOptions = {
@@ -50,6 +59,6 @@ export default async function handler(req, res) {
 
   if (req.method === 'PATCH') {
     const activeFocusSession = await getActiveFocusSession({ options })
-    await updateActiveFocusSession({ activeFocusSession, options, res })
+    await pauseActiveFocusSession({ activeFocusSession, options, res })
   }
 }
