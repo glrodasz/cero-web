d01ab24fb8434c1a708ecb40f0097e0c5f330023
Guillermo Rodas <guillermo.rodas@eqtpartners.com>
2025-12-30 11:40:10 +0100

    chore: commit changes

--- pages/api/local/tasks/index.js
+++ pages/api/local/tasks/index.js
@@ -64,7 +64,7 @@ export default async function handler(req, res) {
         status,
         priority: 0,
         description,
-        focusSessionId: activeFocusSession.id,
+        focusSessionId: activeFocusSession?.id ?? null,
         createdAt: Date.now(),
       },
     }
